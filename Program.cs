7a9773e93a4361672c6abb83444e60314a3007fe
Emmanuel Mendoza <emmanuelmendoza448@gmail.com>
2025-06-05 21:26:31 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                var response = await client.GetAsync("https://randomuser.me/api/");
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"Respuesta JSON de la API: {json}");
+                }
+                else
+                {
+                    Console.WriteLine($"Error en la solicitud: {response.StatusCode}");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error en la solicitud: {ex.Message}");
+            }
+        }
+    }
+}
