6a938ff23bbdd10280487fc3eef35ab8db6b921e
Emmanuel Mendoza <emmanuelmendoza448@gmail.com>
2025-06-06 20:39:09 -0600

    se agrega una lista para almacenar varias consultas

--- Program.cs
+++ Program.cs
@@ -10,17 +10,31 @@ class Program
         using HttpClient client = new HttpClient();
         try
         {
-            var response = await client.GetAsync("https://randomuser.me/api/");
-            if (response.IsSuccessStatusCode)
+            //se crea una lsita para almacenar las consultas
+            List<UserResponse> listaResultados = new List<UserResponse>();
+            //se realizan 3 consultas
+            for (int i = 0; i < 3; i++)
             {
-                string json = await response.Content.ReadAsStringAsync();
-                var userData = JsonSerializer.Deserialize<UserResponse>(json);
+                var response = await client.GetAsync("https://randomuser.me/api/");
+                if (response.IsSuccessStatusCode)
+
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    var userData = JsonSerializer.Deserialize<UserResponse>(json);
+                    listaResultados.Add(userData);
+                }
+            }
+
+            //se obtienen los datos de cada una de las 3 consultas
+            foreach (var userData in listaResultados)
+            {
                 Console.WriteLine($"Nombre: {userData.results[0].name.first} {userData.results[0].name.last}");
                 Console.WriteLine($"Ciudad: {userData.results[0].location.city}, {userData.results[0].location.country}");
                 Console.WriteLine($"Email: {userData.results[0].email}");
                 Console.WriteLine($"Edad: {userData.results[0].dob.age}");
-                Console.WriteLine($"Foto: {userData.results[0].picture.large}");
+                Console.WriteLine($"Foto: {userData.results[0].picture.large}\n\n");
             }
+
         }
         catch (Exception ex)
         {
